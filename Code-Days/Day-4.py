9db093f55726d9d62146f4ca24280c2c1509cee3
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-08-26 08:31:49 -0400

    Create Day-4.py

---
+++ Code-Days/Day-4.py
@@ -0,0 +1,31 @@
+#  Create your own story using concatenate, print, strings, input, and colors! Was pretty fun! 
+
+print("A story of a faraway princess..")
+print()
+print("Let's learn about this princess... YOU!")
+print()
+your_name = input("What is your name, Doll?: ")
+your_enemy = input("Who would you consider your enemy?: ")
+your_dream = input(
+    "All royalty has dreams of being something else. What's yours?: ")
+your_pet = input("What is your lucky companion?: ")
+pet_name = input("What's their name?: ")
+your_weapon = input(
+    "This is a must have for all adventures. Choose your weapon: ")
+your_lover = input("A lover, a crush.. Name them!: ")
+special_lover = input("What's so special about your lover anyways?: ")
+print()
+print("Let the story commence...")
+print()
+print()
+print(
+    your_name, "was often very lonely. They often dreamed of", your_dream,
+    "but dreams are only that. They had been locked away to protect them from",
+    your_enemy, ",their worst nemesis. (Reasons unknonwn)")
+print()
+print("Luckily,", your_name, "had a trusty friend,", your_pet, "named",
+      pet_name, "who was always by their side during these times.", your_name,
+      "also had", your_weapon,
+      "to keep", "\033[35m","them warm at night.","\033[0m" ,"Of course this can't be compared to",
+      your_lover, "who just so happens to be the best at", special_lover,
+      "oh how our princess suffers alone..")
