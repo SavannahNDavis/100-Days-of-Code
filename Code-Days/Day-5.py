4b44fbaf22cc307c5abc1b0f4d9541768b66f92b
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-08-27 08:14:56 -0400

    Create Day-5.py

---
+++ Code-Days/Day-5.py
@@ -0,0 +1,15 @@
+#  If statements! Finally learned this!
+
+my_name = input("Enter your name: ")
+if my_name == "Savannah":
+  print("Hello, my Savannah.")
+  print("Your computer has missed you. Glad to see you back.")
+if my_name == "Layla":
+  print("Hello, Layla.")
+  print("I hope you're not up to anything suspicous..")
+  print("You are being monitored.")
+if my_name == "Madisen":
+  print("Oh.. it's you..")
+  print("What's for dinner, woman!")
+else: 
+  print("ERROR - ERROR")
