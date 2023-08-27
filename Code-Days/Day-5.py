746cc42b04abdb2638f71af9cfe1592139ec4eaa
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-08-27 08:54:45 -0400

    Update Day-5.py
    added in day 5 challenge. it was pretty hard honestly.

--- Code-Days/Day-5.py
+++ Code-Days/Day-5.py
@@ -1,7 +1,7 @@
-#  If statements! Finally learned this!
+#  If statements! Finally learned this! Wrote my own if statements! 
 
 my_name = input("Enter your name: ")
-if my_name == "Savannah":
+if my_name == "Savannah":  # always have your double == & the colon! 
   print("Hello, my Savannah.")
   print("Your computer has missed you. Glad to see you back.")
 if my_name == "Layla":
@@ -11,7 +11,7 @@ if my_name == "Layla":
 if my_name == "Madisen":
   print("Oh.. it's you..")
   print("What's for dinner, woman!")
-else: 
+else:     #else should NOT be indented, but on the same line as if.
   print("ERROR - ERROR")
 
 #Fix my code - Code with ERRORS
@@ -29,3 +29,19 @@ if drink == "coffee":
   print("Tea is better.")
 else:
   print("Excellent choice.")
+
+#Day 5 Challenge
+
+print("Hello! Let's identify you..")
+human = input("Stay human or turn?:")
+if human == "turn":
+  print("continue on..")
+if human == "stay human":
+  print("this is not the quiz for you.")
+if human == "human":
+  print("this is not the quiz for you.")
+bella = input("Do you like the color brown & you're pretty plain?:")
+if bella == "yes":
+  print("Where the hell you been, loca?!")
+if bella == "no":
+  print("oh, you're not loca.")
