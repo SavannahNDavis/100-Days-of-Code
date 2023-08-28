68a2522274070922eb0a1fcfb47df3fbff3d00bd
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-08-28 08:27:12 -0400

    Update Day-6.py
    Added in the fix my code challenge!

--- Code-Days/Day-6.py
+++ Code-Days/Day-6.py
@@ -12,3 +12,30 @@ elif username == "Bookie" and password == "irule":
   print("Hello, Bookie.")
 else:
   print("Go away!")
+
+#  Fix my code challenge:
+#  season = input(what is your favorite season?)
+# if season = "spring"
+ #  print("Ah! The birds are chirping and flowers blooming.")
+ # elif season == summer:
+ # elif season == autumn
+# print("The leaves are changing and the air is crisp. Enjoy!)
+      # elif season = winter:
+      # print("Stay warm by the fire and watch the snow fall.")
+# else: 
+  # print("I don't know that season. Please try again.")
+
+
+
+#  Fixed the code!:
+season = input("what is your favorite season?")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
