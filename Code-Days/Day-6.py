b732f4203b5dfe4ae44c565998434272d3370c72
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-08-28 08:20:44 -0400

    Create Day-6.py

---
+++ Code-Days/Day-6.py
@@ -0,0 +1,14 @@
+# elif 
+
+#practice
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password >")
+if username == "mark" and password == "password":
+  print("Welcome Mark!")
+elif username == "sav" and password == "nope":
+  print("Hello, Savannah")
+elif username == "Bookie" and password == "irule":
+  print("Hello, Bookie.")
+else:
+  print("Go away!")
