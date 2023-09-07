5fa47dc89e3f4ca92112962133d6408d0040927f
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-09-07 08:55:22 -0400

    Update Day-11.py
    added in mil_year & mil_leap for military length.

--- Code-Days/Day-11.py
+++ Code-Days/Day-11.py
@@ -4,18 +4,19 @@ name = input("Hello! Who is heading to the military?: ")
 year = int(input("What year are they heading in?: "))
 leap_year = input("Is it a leap year?: ")
 
-calc_seconds = .60
+calc_seconds = 60
 calc_minutes = 60
 calc_hours = 24
 calc_month = 12
 calc_year = 365
 calc_leap = 366
-
+mil_year = calc_year * 2 
+mil_leap = calc_leap * 2
 
 if leap_year == "yes" or "Yes":
-  total_leap = calc_minutes * calc_seconds * calc_hours * calc_month * calc_leap
+  total_leap = calc_minutes * calc_seconds * calc_hours * calc_month * mil_leap
   print("Don't feel too sad.", name," will be back in ",total_leap, "seconds.")
 else:
-  total_year = calc_minutes * calc_seconds * calc_hours * calc_month * calc_year
+  total_year = calc_minutes * calc_seconds * calc_hours * calc_month * mil_year
   print("Don't feel too sad.", name," will be back in ",total_year, "seconds.")
 
