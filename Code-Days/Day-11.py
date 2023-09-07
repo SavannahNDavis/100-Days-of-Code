9f29f7f4d74723fe7babd6a93cefd954ea49d5da
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-09-07 08:51:34 -0400

    Update Day-11.py
    Added in day 11 challenge. Math..

--- Code-Days/Day-11.py
+++ Code-Days/Day-11.py
@@ -1,1 +1,21 @@
+# Day 11 Challenge - How many minutes seconds in a year? I'll spin this to how many seconds until Suga is back from the military.
+
+name = input("Hello! Who is heading to the military?: ")
+year = int(input("What year are they heading in?: "))
+leap_year = input("Is it a leap year?: ")
+
+calc_seconds = .60
+calc_minutes = 60
+calc_hours = 24
+calc_month = 12
+calc_year = 365
+calc_leap = 366
+
+
+if leap_year == "yes" or "Yes":
+  total_leap = calc_minutes * calc_seconds * calc_hours * calc_month * calc_leap
+  print("Don't feel too sad.", name," will be back in ",total_leap, "seconds.")
+else:
+  total_year = calc_minutes * calc_seconds * calc_hours * calc_month * calc_year
+  print("Don't feel too sad.", name," will be back in ",total_year, "seconds.")
 
