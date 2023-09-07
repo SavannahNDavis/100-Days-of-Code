ca2f77290ecebe6eb024ded86008db9560530526
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-09-07 08:24:01 -0400

    Update 10.py

--- Code-Days/10.py
+++ Code-Days/10.py
@@ -1,1 +1,11 @@
+# Day 10 Challenge! 
 
+total_bill = float(input("Whats the bill total?: "))
+tip = int(input("What would you like to tip?: "))
+total_people = int(input("How many people are there?: "))
+tip_calc = tip /100
+bill_tip = total_bill * tip_calc
+total = bill_tip + total_bill
+answer = total/total_people
+answer = round(answer,2)
+print("You all owe,",answer)
