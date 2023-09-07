111cbff0b88c4e289dad89b32031636603b3538e
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-09-06 22:11:36 -0400

    Create Day-9.py

---
+++ Code-Days/Day-9.py
@@ -0,0 +1,24 @@
+# Day 9! Float & Int
+
+# Fix my code - Problem Code
+
+score = input("What was your score on the test?"))
+if score >= 80
+  print("Not too shabby")
+elif score > "70":
+  print("Acceptable.")
+else:
+print("Dude, you need to study more!")
+
+# Fixed Code! 
+
+score = int(input("What was your score on the test?"))
+if score >= 80 :
+  print("Not too shabby")
+elif score >= 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
+  
+# Day 9 Challenge
+
