fd9b4a92140e72ef7f6698fbfd158db7d86c5b57
Savannah Davis <142052869+SavannahNDavis@users.noreply.github.com>
2023-09-06 23:09:18 -0400

    Update Day-9.py

--- Code-Days/Day-9.py
+++ Code-Days/Day-9.py
@@ -22,3 +22,12 @@ else:
   
 # Day 9 Challenge
 
+print("Let's find out what Gen you were born in!")
+print()
+birth_year = int(input("What year were you born?: "))
+if birth_year == 1925:
+  print(" You're like really old. Are you okay? Also You're a traditionalist.")
+elif birth_year <= 1946:
+    print("Yikes.. A tradionalist. Can't say I'm impressed, oldie.")
+else:
+  print("So I don't know what gen you are haha")
